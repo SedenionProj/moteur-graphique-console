3ab7088428e0708833320f33f6f1cca923955718
SedenionProj <96470946+SedenionProj@users.noreply.github.com>
2023-09-24 11:27:47 +0200

    Update moteur_graphique.py

--- moteur_graphique.py
+++ moteur_graphique.py
@@ -3,7 +3,7 @@ from lib_math import *
 
 width,height = os.get_terminal_size()
 height -= 1
-pixelBuffer = ['*']*(width*height)
+pixelBuffer = [' ']*(width*height)
 
 def draw():
     print(''.join(pixelBuffer),end='')
@@ -18,7 +18,37 @@ def putPixel(v, char):
     if 0 <=px<width and 0<=py<height:
         pixelBuffer[py * width + px] = char
 
-putPixel(vec2(10,10), " ")
-draw()
+def putTriangle(tri,char):
+    def eq(p, a, b):
+        return (a.x-p.x)*(b.y-p.y)-(a.y-p.y)*(b.x-p.x)
+
+    xmin = round(min(tri.p1.x, tri.p2.x, tri.p3.x))
+    xmax = round(max(tri.p1.x, tri.p2.x, tri.p3.x))+1
+    ymin = round(min(tri.p1.y, tri.p2.y, tri.p3.y))
+    ymax = round(max(tri.p1.y, tri.p2.y, tri.p3.y))+1
+    for y in range(ymin,ymax):
+        if 0<=y<height:
+            for x in range(xmin,xmax):
+                if 0<=x<width:
+                    pos = vec2(x,y)
+                    w1 = eq(pos, tri.p3, tri.p1)
+                    w2 = eq(pos, tri.p1, tri.p2)
+                    w3 = eq(pos, tri.p2, tri.p3)
+                    if (w1 >= 0 and w2 >= 0 and w3 >= 0) or (-w1 >= 0 and -w2 >= 0 and -w3 >= 0):
+                        putPixel(pos,char)
+
+
+tri = triangle(vec2(10,10),
+               vec2(80,15),
+               vec2(40,30))
+
+tri2 = triangle(vec2(80,30),
+               vec2(100,35),
+               vec2(90,50))
+while True:
+    clear(' ')
+    putTriangle(tri,'@')
+    putTriangle(tri2,'@')
+    draw()
 
 input()
